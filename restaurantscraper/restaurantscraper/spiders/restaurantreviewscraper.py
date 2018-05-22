2fff1f2e3c27d8390e345f432c09908b3a2451d9
Akshit Jain <akshit24x7@yahoo.com>
2018-05-22 18:42:12 +0530

    v0.0.5

--- restaurantscraper/restaurantscraper/spiders/restaurantreviewscraper.py
+++ restaurantscraper/restaurantscraper/spiders/restaurantreviewscraper.py
@@ -5,15 +5,19 @@ import scrapy
 class RestaurantreviewscraperSpider(scrapy.Spider):
 	name = 'restaurantreviewscraper'
 	allowed_domains = ['tripadvisor.in']
-	start_urls = ['https://www.tripadvisor.in/Restaurants-g304554-Mumbai_Maharashtra.html',
-				'https://www.tripadvisor.in/Restaurants-g304551-New_Delhi_National_Capital_Territory_of_Delhi.html',
-				]
+	start_urls = ['https://www.tripadvisor.in/Restaurants-g304554-Mumbai_Maharashtra.html']
 	
 	def parse(self, response):
 		for restaurant in response.css('a.property_title'):
 			res_url = ('https://www.tripadvisor.in%s' % restaurant.xpath('@href').extract_first())
 			yield scrapy.Request(res_url, callback=self.parse_restaurant)
-			return
+
+		next_page = ('https://www.tripadvisor.in%s'\
+			% (response.css('a.nav.next.rndBtn.ui_button.primary.taLnk')).xpath('@href').extract_first())
+		print('NEXT PAGE: ' + next_page)
+		if next_page:
+			yield scrapy.Request(next_page, callback=self.parse)
+		
 	def parse_restaurant(self, response):
 		print(response.xpath('//div[@class="blEntry phone"]//span/text()').extract_first())
         
