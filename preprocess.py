f01d42a3ecc0a38ad36acf5834aee5bf1398139a
Akshit Jain <akshit24x7@yahoo.com>
2018-05-29 20:20:30 +0530

    Preprocess v0.0.1

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 class PreprocessRestaurantItem(object):
 	
-	db_name = 'restaurantinfo'	
+	db_name = 'restaurantinfo'
+	fields = ['rest_name', 'rest_rank', 'rest_addr', 'rest_rating', 'rest_price', 'rest_total_reviews', 'rest_reviews']
 
 	def __init__(self): 
 		self.client = MongoClient('mongodb://localhost:27017/restaurantinfo')
@@ -13,10 +14,13 @@ class PreprocessRestaurantItem(object):
 	def load_mongodb_to_pandas(self):	
 		restaurants = []
 		for doc in self.db.restaurantreviews.find():
-			restaurants.append([doc['rest_name'], doc['rest_rank']])
-		df = pd.DataFrame(restaurants, columns=['rest_name', 'rest_rank'])
-		print(restaurants)
+			restaurants.append([doc['rest_name'], doc['rest_rank'], doc['rest_addr'],
+								doc['rest_rating'], doc['rest_price'],
+								doc['rest_total_reviews'], doc['rest_reviews']])
+		df = pd.DataFrame(restaurants, columns=self.fields)
 		print(df)
+
+	# TODO: cleaning and preprocessing text reviews
 
 if __name__ == '__main__':
 	preprocess = PreprocessRestaurantItem()
