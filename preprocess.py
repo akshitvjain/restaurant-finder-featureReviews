721120f5b998bd61d4ecb31fa192fbf78aa8561d
Akshit Jain <akshit24x7@yahoo.com>
2018-05-29 18:50:38 +0530

    Preprocess v0.0.0

--- preprocess.py
+++ preprocess.py
@@ -2,13 +2,22 @@
 from pymongo import MongoClient
 import pandas as pd
 
-class PreprocessRestaurantItem(object)
+class PreprocessRestaurantItem(object):
 	
 	db_name = 'restaurantinfo'	
 
 	def __init__(self): 
 		self.client = MongoClient('mongodb://localhost:27017/restaurantinfo')
-		self.db = client[db_name]
+		self.db = self.client[self.db_name]
 	
-	def mongodb_to_pandas(self):
-		pass
+	def load_mongodb_to_pandas(self):	
+		restaurants = []
+		for doc in self.db.restaurantreviews.find():
+			restaurants.append([doc['rest_name'], doc['rest_rank']])
+		df = pd.DataFrame(restaurants, columns=['rest_name', 'rest_rank'])
+		print(restaurants)
+		print(df)
+
+if __name__ == '__main__':
+	preprocess = PreprocessRestaurantItem()
+	preprocess.load_mongodb_to_pandas()
