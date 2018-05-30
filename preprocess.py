df7acf1034d54a61f68565c9abdb101e765b69ee
Akshit Jain <akshit24x7@yahoo.com>
2018-05-30 20:55:07 +0530

    Preprocess v0.0.2

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from pymongo import MongoClient
 import pandas as pd
+import spacy
+nlp = spacy.load('en')
+from spacy.lang.en.stop_words import STOP_WORDS
 
 class PreprocessRestaurantItem(object):
 	
 	db_name = 'restaurantinfo'
 	fields = ['rest_name', 'rest_rank', 'rest_addr', 'rest_rating', 'rest_price', 'rest_total_reviews', 'rest_reviews']
+	df = None
 
 	def __init__(self): 
 		self.client = MongoClient('mongodb://localhost:27017/restaurantinfo')
@@ -17,11 +21,15 @@ class PreprocessRestaurantItem(object):
 			restaurants.append([doc['rest_name'], doc['rest_rank'], doc['rest_addr'],
 								doc['rest_rating'], doc['rest_price'],
 								doc['rest_total_reviews'], doc['rest_reviews']])
-		df = pd.DataFrame(restaurants, columns=self.fields)
-		print(df)
+		self.df = pd.DataFrame(restaurants, columns=self.fields)
 
-	# TODO: cleaning and preprocessing text reviews
+	def preprocess_reviews(self):	
+		pass
+		# TODO: cleaning and preprocessing text reviews
+			
+						
 
 if __name__ == '__main__':
 	preprocess = PreprocessRestaurantItem()
 	preprocess.load_mongodb_to_pandas()
+	preprocess.preprocess_reviews()
