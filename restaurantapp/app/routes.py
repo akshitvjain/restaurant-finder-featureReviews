fb43bc7f349b6299f85c1c1f0ac59c77d32d1d40
Akshit Jain <akshit24x7@yahoo.com>
2018-07-06 17:37:51 +0530

    App v0.0.2

--- restaurantapp/app/routes.py
+++ restaurantapp/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Restaurant Recommender!"
+	user = {'username': 'USER'}
+	return render_template('index.html', user=user)
