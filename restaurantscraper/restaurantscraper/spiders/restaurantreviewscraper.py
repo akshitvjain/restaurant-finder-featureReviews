45e6b8e5e56d08a42e865ea8af8b71b8c2e9fd8b
Akshit Jain <akshit24x7@yahoo.com>
2018-12-04 17:39:26 +0530

    Scraper v0.2.4

--- restaurantscraper/restaurantscraper/spiders/restaurantreviewscraper.py
+++ restaurantscraper/restaurantscraper/spiders/restaurantreviewscraper.py
@@ -39,34 +39,44 @@ class RestaurantreviewscraperSpider(scrapy.Spider):
 	def parse_restaurant(self, response):
 		hasReviews = True
 		sel = Selector(response)
+
 		# start the webdriver to crawl reviews
 		driver = webdriver.Chrome()
+
 		# initialize Item class to access the fields
 		rest_item = RestaurantscraperItem()
+
 		# extract restaurant name
 		rest_item['rest_name'] = sel.xpath('//h1/text()').extract()[1]
+
 		# extract restaurant addr 
 		street = response.xpath('//*[@id="taplc_restaurants_detail_info_content_0"]/div[4]/div[2]/span[2]/text()').extract_first()
 		rest_item['rest_addr'] = street + ", London, England" 
+
 		# extract cuisine info
 		rest_item['rest_cuisines'] = \
-			response.xpath('//*[@id="RESTAURANT_DETAILS"]/div[2]/div[1]/div[3]/div[2]/a/text()').extract()
+			response.xpath('//*[@id="taplc_restaurants_detail_info_content_0"]/div[2]/div/div[2]/div[2]/text()').extract_first()
+
 		# extract restaurant rank
 		if (response.css('div.prw_rup.prw_restaurants_header_eatery_pop_index')):
 			rest_item['rest_rank'] = sel.xpath('//b/span/text()').extract()[0]
 		else:
 			rest_item['rest_rank'] = None
+
 		# extract ratings
 		if (response.css('span.overallRating')):
 			rest_item['rest_rating'] = float(response.css('span.overallRating::text').extract_first())
 		else:
 			rest_item['rest_rating'] = 0
+
 		# extract price
-		if (response.css('span.text')):
+		hasPrice = (response.xpath('//*[@id="RESTAURANT_DETAILS"]/div[2]/div[1]/div[2]/div[1]/text()').extract_first().replace("\n", ""))
+		if (hasPrice == "Average prices"):
 			rest_item['rest_price'] = \
 				response.xpath('//*[@id="RESTAURANT_DETAILS"]/div[2]/div[1]/div[2]/div[2]/span/text()').extract_first()
 		else:
-			rest_item['res_price'] = None
+			rest_item['rest_price'] = None
+
 		# extract number of reviews 
 		if (response.css('a.seeAllReviews')):
 			rest_item['rest_total_reviews'] = \
