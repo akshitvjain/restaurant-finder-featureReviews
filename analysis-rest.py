ca64a96e5467a49fbc64db432b42c0f0db75fb97
Akshit Jain <akshit24x7@yahoo.com>
2018-12-07 18:49:25 +0530

    Analysis v0.0.5

--- analysis-rest.py
+++ analysis-rest.py
@@ -8,9 +8,10 @@ import pandas as pd
 class AnalyzeRestaurantItem(object):
 
 	db_name = 'restaurantinfo'
-	fields = ['rest_name', 'rest_addr', 'lat', 'lon', 'rest_location', 'rest_rating', \
-				'rest_pos_count', 'rest_neg_count', 'rest_total_reviews', \
-				'rest_price', 'rest_cuisines', 'rest_features', 'rest_meals']
+	fields = ['rest_name', 'lat', 'lon', 'rest_street', 'rest_city', \
+				'rest_country', 'rest_rating', 'rest_pos_count', \
+				'rest_neg_count', 'rest_total_reviews', 'rest_price', \
+				'rest_cuisines', 'rest_features', 'rest_meals']
 	df = None
 
 	def __init__(self):
@@ -33,12 +34,15 @@ class AnalyzeRestaurantItem(object):
 
 		rest_info = []
 		for doc in self.db.restaurantreviews.find():
-			lat, lon = self.convert_addr_to_coord(doc['rest_addr'])	
+			street = doc['rest_street']
+			city = doc['rest_city']
+			country = doc['rest_country']
+			lat, lon = self.convert_addr_to_coord(street + ", " + city + ", " + country)	
 			if (lat != 0 and lon != 0):
-				rest_info.append([doc['rest_name'], doc['rest_addr'], float(lat), float(lon), \
-								doc['rest_location'], doc['rest_rating'], doc['rest_pos_count'], \
-								doc['rest_neg_count'], doc['rest_total_reviews'], doc['rest_price'], \
-								doc['rest_cuisines'], doc['rest_features'], doc['rest_meals']])
+				rest_info.append([doc['rest_name'], float(lat), float(lon), doc['rest_street'], \
+								doc['rest_city'], doc['rest_country'], doc['rest_rating'], \
+								doc['rest_pos_count'], doc['rest_neg_count'], doc['rest_total_reviews'], \
+								doc['rest_price'], doc['rest_cuisines'], doc['rest_features'], doc['rest_meals']])
 		self.df = pd.DataFrame(rest_info, columns=self.fields)
 		print(self.df)	
 
