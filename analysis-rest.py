4fc4608cb1aa0a3fd50098256d38acc36e3f614f
Akshit Jain <akshit24x7@yahoo.com>
2018-12-17 12:01:20 +0530

    Prepare Data for Analysis

--- analysis-rest.py
+++ analysis-rest.py
@@ -8,14 +8,14 @@ import pandas as pd
 class AnalyzeRestaurantItem(object):
 
 	db_name = 'restaurantinfo'
-	info_fields = ['rest_name', 'lat', 'lon', 'rest_street', 'rest_city', \
-				'rest_country', 'rest_rating', 'rest_price', 'review_excellent_count', \
-				'review_good_count', 'review_avg_count', 'review_poor_count', \
-				'review_terrible_count', 'positive_review_count', 'negative_review_count', \
-				'rest_total_reviews']
-	cuisine_fields = ['rest_name', 'cuisine']
-	features_fields = ['rest_name', 'rest_features']
-	meals_fields = ['rest_name', 'rest_meals']
+	info_fields = ['restaurant name', 'latitude', 'longitude', 'street', 'city', \
+				'country', 'rating', 'price', 'review excellent count', \
+				'review good count', 'review avg count', 'review poor count', \
+				'review terrible count', 'positive review count', 'negative review count', \
+				'total reviews']
+	cuisine_fields = ['restaurant name', 'cuisine']
+	features_fields = ['restaurant name', 'feature']
+	meals_fields = ['restaurant name', 'meal']
 
 	info_df = None
 	cuisines_df = None
@@ -45,11 +45,17 @@ class AnalyzeRestaurantItem(object):
 		rest_features = []
 		rest_meals = []
 		for doc in self.db.restaurantreviews.find():
+			
 			street = doc['rest_street']
 			city = doc['rest_city']
 			country = doc['rest_country']
 			lat, lon = self.convert_addr_to_coord(street + ", " + city + ", " + country)	
-			if (lat != 0 and lon != 0):
+			
+			if (lat != 0 and lon != 0 and 
+				doc['rest_cuisines'] != None and
+				doc['rest_features'] != None and
+				doc['rest_meals'] != None):
+	
 				positive_review_count = int(doc['review_excellent_count']) + \
 										int(doc['review_good_count'])
 				negative_review_count = int(doc['review_avg_count']) + \
@@ -74,8 +80,8 @@ class AnalyzeRestaurantItem(object):
 								positive_review_count, negative_review_count, doc['rest_total_reviews']])
 
 				# load restaurant cuisines, price data
-				if doc['rest_cuisines']:
-					cuisines = doc['rest_cuisines'].split(',')
+				if doc['rest_cuisines']:		
+					cuisines = doc['rest_cuisines'].split(',')	
 					for cuisine in cuisines:
 						rest_cuisine.append([doc['rest_name'], cuisine.strip("\n")])
 				# load restaurant features
@@ -93,6 +99,11 @@ class AnalyzeRestaurantItem(object):
 		self.cuisines_df = pd.DataFrame(rest_cuisine, columns=self.cuisine_fields)
 		self.features_df = pd.DataFrame(rest_features, columns=self.features_fields)
 		self.meals_df = pd.DataFrame(rest_meals, columns=self.meals_fields)
+
+		self.info_df.to_csv('data/restaurant_info.csv', index=False)
+		self.cuisines_df.to_csv('data/cuisines_info.csv', index=False)
+		self.features_df.to_csv('data/features_info.csv', index=False)
+		self.meals_df.to_csv('data/meals_info.csv', index=False)
 
 		print(self.info_df)	
 		print(self.cuisines_df)
