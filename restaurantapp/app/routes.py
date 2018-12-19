0df10e8ae70e76635f4d092a643bdb22433a9963
Akshit Jain <akshit24x7@yahoo.com>
2018-12-19 18:35:19 +0530

    App v0.0.5

--- restaurantapp/app/routes.py
+++ restaurantapp/app/routes.py
@@ -2,12 +2,17 @@ from pymongo import MongoClient
 from flask import render_template
 from app import app
 
-#client = MongoClient("mongodb://127.0.0.1:27017/restaurantinfo")
-#db = client['restaurantinfo']
+client = MongoClient("mongodb://127.0.0.1:27017/restaurantinfo")
+db = client['restaurantinfo']
 
 @app.route('/')
-@app.route('/index')
-def index():
-	user = {'username': 'Akshit Jain'}
-	#restaurant = db.restaurantreviews.find({"rest_name" : "Amrutha Lounge"})
-	return render_template('index.html', user=user)
+@app.route('/home')
+def home():
+	restaurant = db.restaurantreviews.find({}, {"rest_name":1, "_id":0})
+	for doc in restaurant:
+		print(doc)
+		return render_template('home.html', user=doc)
+@app.route('/search')
+def search():
+	return render_template('search.html')
+
